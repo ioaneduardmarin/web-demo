4ebde345d1d381a8a802e910f38126291394eac0
ioaneduardmarin <58357224+ioaneduardmarin@users.noreply.github.com>
2021-02-12 15:32:05 +0200

    Add explanations

--- home.js
+++ home.js
@@ -1,25 +1,31 @@
 //JavaScript source code
+
+//Update page first load
 onUpdate();
 setTextIfNoToDoItems();
 
+//Trigger addToDoItem to create a li element when the addTodoButton was pressed
 const addButton = document.getElementById('addTodoButton');
 addButton.addEventListener('click', function () {
     addToDoItem(getToDoTextBoxText());
     setTextIfNoToDoItems();
 });
 
+//Trigger addToDoItem to create a li element when the form was submitted on enter key press
 document.getElementById('toDoForm').addEventListener('submit', function (event) {
     event.preventDefault();
     addToDoItem(getToDoTextBoxText());
     setTextIfNoToDoItems();
 });
 
+//Updates the page when the local storage is modified
 window.addEventListener('storage', (e) => {
-    console.log("Declansez operatia de sincronizare a itemilor toDo intre tabele");
+    console.log('Declansez operatia de sincronizare a itemilor toDo intre tabele');
     onUpdate();
     setTextIfNoToDoItems();
 });
 
+//Updates the page on load
 window.addEventListener('load', () => {
     setToDoTextBoxText();
 });
--- utils.js
+++ utils.js
@@ -4,14 +4,71 @@ const app = {
     toDoItems: []
 };
 
+//Add new li elements
+function addToDoItem(toDoItemText) {
+    //Determines the idNumber of the last entered item to determine what Ids will the li element and its children element will have
+    app.idOfLastEnteredToDoItem = window.localStorage.getItem('numberOfItems');
+    app.idOfLastEnteredToDoItem++;
+
+    //Stops action if text is nullOrEmpty
+    if (!toDoItemText) {
+        return;
+    }
+
+    let list = document.getElementById('toDoList');
+
+    //Creates the li element and its children elements
+    let newEntry = prepareToDoItem(app.idOfLastEnteredToDoItem);
+    let toDoItemSpan = prepareToDoItemSpan(toDoItemText, parseInt((newEntry.id).slice(8)));
+    let removalAnchor = prepareToDoItemRemovalAnchor(parseInt((newEntry.id).slice(8)));
+
+    //Updates the idNumber of the last entered item
+    window.localStorage.setItem('numberOfItems', parseInt((newEntry.id).slice(8)));
+
+    //Appends children elements to parent li elements
+    newEntry.appendChild(toDoItemSpan);
+    newEntry.appendChild(removalAnchor);
+
+    //Commit changes after the element loses focus
+    toDoItemSpan.addEventListener('blur', function (event) {
+        event.preventDefault();
+        editToDoItem(toDoItemSpan.id);
+        setTextIfNoToDoItems();
+    });
+
+    //Delete li element after anchor is clicked
+    removalAnchor.addEventListener('click', function (event) {
+        event.preventDefault();
+        deleteToDoItem(newEntry.id);
+        setTextIfNoToDoItems();
+    });
+
+
+    //Appends li element to its parent ul element and updates the page
+    list.appendChild(newEntry);
+    setToDoTextBoxText();
+
+    //Appends important element data to local storage
+    let toDoObject = { toDoId: newEntry.id, contentText: toDoItemSpan.textContent };
+    window.localStorage.setItem(toDoObject.toDoId, toDoObject.contentText);
+}
+
+//Gets text from input element
 function getToDoTextBoxText() {
     return document.getElementById('addTodoTextBox').value;
 }
 
+//Resets input text content state to empty
 function setToDoTextBoxText() {
     document.getElementById('addTodoTextBox').value = '';
 }
 
+//Sets the visibility state of the element with id equal to 'noToDoItemInfo'
+function setTextIfNoToDoItems() {
+    document.getElementById('noToDoItemInfo').hidden = doTodoItemsExist();
+}
+
+//Checkhs if the ul element has children elemens
 function doTodoItemsExist() {
     let number = document.getElementById('toDoList').childElementCount;
     if (parseInt(number) > 0) {
@@ -20,77 +77,26 @@ function doTodoItemsExist() {
     return false;
 }
 
-function setTextIfNoToDoItems() {
-    document.getElementById('noToDoItemInfo').hidden = doTodoItemsExist();
+//Creates the li element
+function prepareToDoItem(id) {
+    let newEntry = document.createElement('li');
+    newEntry.id = `toDoItem${id}`;
+    newEntry.className = 'toDoItem';
+    return newEntry;
 }
 
-function addToDoItem(toDoItemText) {
-    app.idOfLastEnteredToDoItem = window.localStorage.getItem('numberOfItems');
-    app.idOfLastEnteredToDoItem++;
-    if (!toDoItemText) {
-        return;
-    }
-
-    let list = document.getElementById('toDoList');
-    let newEntry = prepareToDoItem(app.idOfLastEnteredToDoItem);
-    let toDoItemSpan = prepareToDoItemSpan(toDoItemText, parseInt((newEntry.id).slice(8)));
-    let removalAnchor = prepareToDoItemRemovalAnchor(parseInt((newEntry.id).slice(8)));
-    window.localStorage.setItem('numberOfItems', parseInt((newEntry.id).slice(8)));
-
-    newEntry.appendChild(toDoItemSpan);
-    newEntry.appendChild(removalAnchor);
-
-    toDoItemSpan.addEventListener('blur', function (event) {
-        editToDoItem(toDoItemSpan.id);
-        event.preventDefault();
-        setTextIfNoToDoItems();
-    });
-
-    removalAnchor.addEventListener('click', function (event) {
-        deleteToDoItem(newEntry.id);
-        event.preventDefault();
-        setTextIfNoToDoItems();
-    });
-
-    let toDoObject = { toDoId: newEntry.id, contentText: toDoItemSpan.textContent };
-    list.appendChild(newEntry);
-    setToDoTextBoxText();
-
-    console.log(`Adaug un item nou in localStorage ${toDoObject.toDoId} ${toDoObject.contentText}`);
-    console.log(app.idOfLastEnteredToDoItem);
-    window.localStorage.setItem(toDoObject.toDoId, toDoObject.contentText);
+//Creates the span child element of the li element
+function prepareToDoItemSpan(toDoItemText, idNumber) {
+    let span = document.createElement('span');
+    span.class = 'toDoItemText';
+    span.id = `toDoItemSpan${idNumber}`;
+    span.textContent = toDoItemText;
+    span.contentEditable = true;
+    span.spellcheck = false;
+    return span;
 }
 
-function addUpdatedToDoItems(toDoItemId, toDoItemText) {
-    console.log(`Adaug doar in ul al noului tabel elementul ${toDoItemId} ${toDoItemText}`);
-    const idNumber = parseInt(toDoItemId.slice(8))
-    app.idOfLastEnteredToDoItem = idNumber;
-    let list = document.getElementById('toDoList');
-    let newEntry = prepareToDoItem(idNumber);
-    let toDoItemSpan = prepareToDoItemSpan(toDoItemText, idNumber);
-    let removalAnchor = prepareToDoItemRemovalAnchor(idNumber);
-
-    newEntry.appendChild(toDoItemSpan);
-    newEntry.appendChild(removalAnchor);
-
-    toDoItemSpan.addEventListener('blur',
-        function (event) {
-            editToDoItem(toDoItemSpan.id);
-            event.preventDefault();
-            setTextIfNoToDoItems();
-        });
-
-    removalAnchor.addEventListener('click',
-        function (event) {
-            deleteToDoItem(newEntry.id);
-            event.preventDefault();
-            setTextIfNoToDoItems();
-        });
-
-    list.appendChild(newEntry);
-    setToDoTextBoxText();
-}
-
+//Creates the anchor child element of the li element
 function prepareToDoItemRemovalAnchor(idNumber) {
     let anchor = document.createElement('a');
     anchor.style.padding = "3px 3px 3px 5px";
@@ -102,69 +108,49 @@ function prepareToDoItemRemovalAnchor(idNumber) {
     return anchor;
 }
 
-function prepareToDoItem(id) {
-    let newEntry = document.createElement('li');
-    newEntry.id = `toDoItem${id}`;
-    newEntry.className = 'toDoItem';
-    return newEntry;
-}
-
-function prepareToDoItemSpan(toDoItemText, idNumber) {
-
-    let span = document.createElement('span');
-    span.class = 'toDoItemText';
-    span.id = `toDoItemSpan${idNumber}`;
-    span.textContent = toDoItemText;
-    span.contentEditable = true;
-    span.spellcheck = false;
-    return span;
-}
-
+//Deletes the item found with the given id
 function deleteToDoItem(id) {
-    console.log(localStorage);
     let toBeDeleted = document.getElementById(id);
     if (!toBeDeleted) {
         return;
     }
 
     let toDoObject = { toDoId: id, contentText: toBeDeleted.firstChild.textContent };
-    console.log(`Elimin un item din localStorage ${toDoObject.toDoId} ${toDoObject.contentText}`);
     toBeDeleted.parentNode.removeChild(toBeDeleted);
     window.localStorage.removeItem(id);
-    console.log(app.idOfLastEnteredToDoItem);
-    console.log(localStorage);
 }
 
+//Edits the span item found with the given id or deletes its parent if span element is empty after edit
 function editToDoItem(id) {
     let toBeEdited = document.getElementById(id);
     let toDoObject = { toDoId: id, contentText: toBeEdited.textContent };
     if (toDoObject.contentText) {
-        console.log(`Editez un item  din localStorage ${toDoObject.toDoId} ${toDoObject.contentText}`);
         window.localStorage.setItem(document.getElementById(id).parentNode.id, toDoObject.contentText);
-        console.log(app.idOfLastEnteredToDoItem);
         return;
     }
-    console.log(`Elimin un item din localStorage ${toDoObject.toDoId} ${toDoObject.contentText}`);
     window.localStorage.removeItem(document.getElementById(id).parentNode.id);
     deleteToDoItem(document.getElementById(id).parentNode.id);
-    console.log(app.idOfLastEnteredToDoItem);
 }
 
-function getCurrentDate() {
-    let date = new Date();
-    return date.toLocaleString();
-}
-
-function onLoad() {
-    app.idOfLastEnteredToDoItem = 0;
+//Updates the list of li elements
+function onUpdate() {
+    popElements();
     const myStorage = getMyStorage();
-    myStorage.forEach(element => addToDoItem(element));
+    myStorage.forEach(element => addUpdatedToDoItems(element.toDoId, element.contentText));
 }
 
+//Deletes al li elements
+function popElements() {
+    let toDoItems = document.querySelectorAll('#toDoList > .toDoItem');
+    toDoItems.forEach(element => element.parentNode.removeChild(element));
+}
+
+
+//Gets the objects that contain important data of the existing li elements from local storage
 function getMyStorage() {
 
-    const toDoObjects = [];
-    console.log("Obtin cheile itemilor din localStorage");
+
+    //Extracts keys of the local storage objects and orders them 
     let keys = Object.keys(localStorage);
     keys = keys.filter(key => key.includes('toDoItem'));
     const keyNumbers = [];
@@ -172,7 +158,9 @@ function getMyStorage() {
     keyNumbers.sort(function (a, b) {
         return a - b;
     });
-    console.log("Adaug in vectorul values valorile itemilor din localStorage folosind cheile extrase anterior");
+
+    //Fills the toDoObject array with the sorted local storage objects that contain important data of the existing li elements
+    const toDoObjects = [];
     keyNumbers.forEach(k => {
         let toDoObject = {
             toDoId: `toDoItem${k}`,
@@ -183,14 +171,38 @@ function getMyStorage() {
     return toDoObjects;
 }
 
-function onUpdate() {
-    console.log(app.idOfLastEnteredToDoItem);
-    popElements();
-    const myStorage = getMyStorage();
-    myStorage.forEach(element => addUpdatedToDoItems(element.toDoId, element.contentText));
-}
+//Creates li and its children elements and adds them to the ul element(used when the page is refreshed and when the local storage was modified)
+function addUpdatedToDoItems(toDoItemId, toDoItemText) {
+    const idNumber = parseInt(toDoItemId.slice(8));
+    app.idOfLastEnteredToDoItem = idNumber;
+    let list = document.getElementById('toDoList');
 
-function popElements() {
-    let toDoItems = document.querySelectorAll('#toDoList > .toDoItem');
-    toDoItems.forEach(element => element.parentNode.removeChild(element));
+    //Creates the li element and its children elements
+    let newEntry = prepareToDoItem(idNumber);
+    let toDoItemSpan = prepareToDoItemSpan(toDoItemText, idNumber);
+    let removalAnchor = prepareToDoItemRemovalAnchor(idNumber);
+
+    //Appends children elements to parent li elements
+    newEntry.appendChild(toDoItemSpan);
+    newEntry.appendChild(removalAnchor);
+
+    //Commit changes after the element loses focus
+    toDoItemSpan.addEventListener('blur',
+        function (event) {
+            event.preventDefault();
+            editToDoItem(toDoItemSpan.id);
+            setTextIfNoToDoItems();
+        });
+
+    //Delete li element after anchor is clicked
+    removalAnchor.addEventListener('click',
+        function (event) {
+            event.preventDefault();
+            deleteToDoItem(newEntry.id);
+            setTextIfNoToDoItems();
+        });
+
+    //Appends li element to its parent ul element and updates the page
+    list.appendChild(newEntry);
+    setToDoTextBoxText();
 }
