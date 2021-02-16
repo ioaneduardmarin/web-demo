f57b4503e924007c92e53eb09c5737546cf6665d
ioaneduardmarin <58357224+ioaneduardmarin@users.noreply.github.com>
2021-02-16 16:00:50 +0200

    Add Toastr Notifications

--- home.js
+++ home.js
@@ -18,6 +18,53 @@ document.getElementById('inputForm').addEventListener('submit', function (event)
     setTextIfNoToDoItems();
 });
 
+
+$('#addTodoButton').click(function () {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "1000000",
+        "hideDuration": "1000000",
+        "timeOut": "1000000",
+        "extendedTimeOut": "1000000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    toastr["success"]("Item succesfully added!", "Success!");
+});
+
+$('#inputForm').submit(
+    function () {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-bottom-center",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "3000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+        toastr["success"]("Item succesfully added!", "Success!");
+    });
+
+
 //Updates the page when the local storage is modified
 window.addEventListener('storage', (e) => {
     console.log('Declansez operatia de sincronizare a itemilor toDo intre tabele');
