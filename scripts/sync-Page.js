6b3702317be16b74e94a84e010fe21d11199c529
ioaneduardmarin <58357224+ioaneduardmarin@users.noreply.github.com>
2021-02-22 11:37:37 +0200

    Update sync-Page.js

--- scripts/sync-Page.js
+++ scripts/sync-Page.js
@@ -1,5 +1,5 @@
 //Updates the list of div elements
-function onUpdate(args,animationClass) {
+function onUpdate(args, animationClass) {
     popElements();
     app.toDoItems = getMyStorage(args);
     app.toDoItems.forEach(element => addUpdatedToDoItems(element.toDoId, element.contentText, element.toDoCheckBoxValue, element.toDoId === app.idOfLastModifiedItem ? `${animationClass}` : ''));
@@ -20,7 +20,7 @@ function getMyStorage(args) {
         let keys = Object.keys(localStorage);
         keys = keys.filter(key => key.includes('toDoItem'));
         keys.forEach(key => keyNumbers.push(parseInt(key.slice(8))));
-        keyNumbers.sort(function(a, b) {
+        keyNumbers.sort(function (a, b) {
             return a - b;
         });
     } else {
@@ -33,15 +33,23 @@ function getMyStorage(args) {
 
     //Fills the toDoObject array with the sorted local storage objects that contain important data of the existing div elements
     const toDoObjects = [];
+    let toDoObject;
     let priorityToDoObjects = [];
     keyNumbers.forEach(k => {
-        let toDoObject = JSON.parse(localStorage.getItem(`toDoItem${k}`));
+
+        if (args === 'localStorageArgs') {
+            toDoObject = JSON.parse(localStorage.getItem(`toDoItem${k}`));
+        } else {
+            toDoObject = app.toDoItems.filter(object => object.toDoId === `toDoItem${k}`)[0];
+        }
+
         if (toDoObject.checkingOrderNumber !== null) {
             priorityToDoObjects.push(toDoObject);
         }
         else {
             toDoObjects.push(toDoObject);
         }
+
         priorityToDoObjects =
             priorityToDoObjects.sort((a, b) => parseInt(a.checkingOrderNumber) - parseInt(b.checkingOrderNumber));
     });
